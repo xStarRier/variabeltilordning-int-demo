9206dc89059c639a6068728b1f5a5a9619c289ee
xStarRier <stephanniequynh@gmail.com>
2024-11-26 09:12:35 +0100

    Add project files.

---
+++ variabeltilordning int demo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sum;
+
+int NumberFunc()
+{
+
+    Console.WriteLine("Insert Number Please");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Insert Number Please");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    sum = num1 + num2;
+
+    return sum;
+
+}
+void Run()
+{
+    NumberFunc();
+    Console.WriteLine($"The summary is: {sum}!");
+    Console.WriteLine($"Thank you for typing a number:)");
+}
+Run();
